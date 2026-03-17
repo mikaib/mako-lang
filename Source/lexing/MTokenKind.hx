4ef19ca108ab70006a3fd75a3a5bfbc939755271
Flawake <60014996+Flawake@users.noreply.github.com>
2026-03-17 12:00:39 +0100

    Add initial tokens

--- Source/lexing/MTokenKind.hx
+++ Source/lexing/MTokenKind.hx
@@ -3,6 +3,31 @@ package lexing;
 import core.MConst;
 
 enum MTokenKind {
-    TSlash;
+    MTokenOperator;
     TConst(const: MConst);
+    TFunc;
+    TParantOpen;
+    TParantClose;
+    TBracketOpen;
+    TBracketClose;
+    TColon;
+    TSemiColon;
+}
+
+enum MTokenOperator {
+    // Unary operators
+    TInrement;
+    TDecrement;
+    TPlus;
+    TMinus;
+    TDivide;
+    TMultiply;
+    TAssign;
+    TEqual;
+
+    // Binary operators
+    TBinOR;
+    TBinXOR;
+    TBinAND;
+    TBinNOT;
 }
