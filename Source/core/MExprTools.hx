0eeed7e4badaf5d8844586d9f3e29d7ec5b2fa13
mikaib <mikaib@duck.com>
2026-03-17 12:12:10 +0100

    iterate tool

---
+++ Source/core/MExprTools.hx
@@ -0,0 +1,35 @@
+package core;
+
+import parsing.MExpr;
+import parsing.MExprList;
+
+class MExprTools {
+
+    public static extern inline overload function iterate(list: MExpr, callback: MExpr->Void): Void {
+        _iterateExpr(list, callback);
+    }
+
+    private static function _iterateExpr(expr: MExpr, callback: MExpr->Void): Void {
+        final invoke = (e: MExpr) -> {
+            callback(e);
+            _iterateExpr(e, callback);
+        };
+
+        switch expr.kind {
+            case EBlock(list): for (e in list) invoke(e);
+            case EBinop(e0, e1, _): invoke(e0); invoke(e1);
+            case EConst(_): null;
+        }
+    }
+
+    public static extern inline overload function iterate(list: MExprList, callback: MExpr->Void): Void {
+        _iterateList(list, callback);
+    }
+
+    private static function _iterateList(list: MExprList, callback: MExpr->Void): Void {
+        for (e in list) {
+            _iterateExpr(e, callback);
+        }
+    }
+
+}
---
+++ Source/typing/MTypeSystem.hx
@@ -0,0 +1,15 @@
+package typing;
+
+import parsing.MExprList;
+
+class MTypeSystem {
+
+    private var _root: MExprList;
+
+    public function new(root: MExprList) {
+        iterate(root);
+    }
+
+    public function iterate
+
+}
