42203408affa4ad3973afdec4668ec4e2c95a4a9
Flawake <60014996+Flawake@users.noreply.github.com>
2026-05-07 16:36:30 +0200

    Fix slight issues

--- Source/parsing/paths/MConstPath.hx
+++ Source/parsing/paths/MConstPath.hx
@@ -4,20 +4,31 @@ import core.MArrayView.ArrayView;
 import lexing.MToken;
 import parsing.MParser.ParserFlowControl;
 import lexing.MTokenKind.TConst;
+import core.MConst;
+import typing.MType;
 
 class MConstPath {
     public static function IntoEConst(input: ArrayView<MToken>): ParserFlowControl {
         var min = input[0];
-        var expr = switch (input[0].kind) {
+        var const= switch (input[0].kind) {
             case TConst(c):
                 input.consume(1);
-                MExprKind.EConst(c);
+                c;
             case _:
                 return PNotParsed;
         }
 
+        var type = switch (const) {
+            case CFloat(_): MType.float(32);
+            case CInt(_): MType.int(32);
+            case CBool(_): MType.bool();
+            case CString(_): MType.string();
+            case _: MType.mono();
+        }
+
         return PReturnSome( {
-            kind: expr,
+            kind: MExprKind.EConst(const),
+            type: type,
             pos: {
                 path: min.pos.path,
                 min: min.pos.min,
